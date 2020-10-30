1a51eb8fb61c90b614227a0d748527c73f8f3135
sangwook <imcool2551@gmail.com>
2020-10-30 09:06:46 +0000

    Updates

---
+++ precache-manifest.df2f04a960a03d7c3ac7c70d094fc643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917963b2c909fc82ba8f50d8d52fc621",
+    "url": "/justhavefun_c/index.html"
+  },
+  {
+    "revision": "7d740c56afff50231e8d",
+    "url": "/justhavefun_c/static/css/main.398726f2.chunk.css"
+  },
+  {
+    "revision": "f26bf2958812d6aade07",
+    "url": "/justhavefun_c/static/js/2.e66bb709.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/justhavefun_c/static/js/2.e66bb709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d740c56afff50231e8d",
+    "url": "/justhavefun_c/static/js/main.220c5d11.chunk.js"
+  },
+  {
+    "revision": "4f09934b9cc2df9c87c5",
+    "url": "/justhavefun_c/static/js/runtime-main.5c768710.js"
+  }
+]);
